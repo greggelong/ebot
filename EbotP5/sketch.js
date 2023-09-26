05cbfdca701a1de188ced4a5159e1601035f5d49
Greg K <gkreisman_cep@qq.com>
2023-09-26 20:43:22 +0800

    move the bar

--- EbotP5/sketch.js
+++ EbotP5/sketch.js
@@ -4,6 +4,7 @@ const isAlpha = str => /^[a-zA-Z]*$/.test(str);
 // great regex is alpha for string check
 // the api fails on 
 let cnv, ustate, pout
+let y=0
 
 function setup() {
   cnv =createCanvas(600, 600);
@@ -29,12 +30,13 @@ function onInput() {
   
   fill("green")
   strokeWeight(10)
-  rect(0, 100, 600, 100)
+  rect(0, y, 600, 100)
   
   // Display the text entered
   fill("black")
   textSize(30)
-  text(ustate.value(), 20, 140)
+  text(ustate.value(), 20, y+50)
+  y+=100
 }
 /*
 for (let [key, value] of Object.entries(yourobject)) {
